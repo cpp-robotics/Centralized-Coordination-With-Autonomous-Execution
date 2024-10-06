f2490c5efbd7875b17d705508d15599d0f4aee07
harrisonchung06 <harrison.chung06@gmail.com>
2024-10-06 15:15:01 -0700

    Add files via upload

--- scanner.py
+++ scanner.py
@@ -3,7 +3,7 @@ import numpy as np
 
 SLICEX = 10
 SLICEY = 10
-E = 20
+E = 50
 
 if __name__ == "__main__":
     cap = cv2.VideoCapture(0)
@@ -20,47 +20,36 @@ if __name__ == "__main__":
         
         top = cy-SLICEY
         bot = cy+SLICEY
-        horzslice = frame[top:bot:,:]
+        horz_slice = frame[top:bot:,:]
 
         left = cx-SLICEX
         right = cx+SLICEX
-        vertslice = frame[:, left:right,:]
+        vert_slice = frame[:, left:right,:]
 
-        #find color
 
-        sumv = np.zeros((1,3))
-        sumh = np.zeros((1,3))
-        
-        for i in range(h):
-            for j in range(SLICEX*2):
-                sumv+=vertslice[i,j,:]
-        
-        for i in range(SLICEY*2):
-            for j in range(w):
-                sumh+=horzslice[i,j,:]
+        random_pixel_vert = vert_slice[int(np.random.rand()*10), int(np.random.rand()*10), :]
 
-        average_color_vert = sumv//(h*SLICEX*2)
-        average_color_horz = sumh//(w*SLICEY*2)
-
-        if average_color_vert[:,0] > 255-E:
+        if np.greater(random_pixel_vert[0], 255-E):
             print('Red detected on vertical')
-        elif average_color_vert[:,1] > 255-E:
+        if np.greater(random_pixel_vert[1], 255-E):
             print('Green detected on vertical')
-        elif average_color_vert[:,2] > 255-E:
-            print('Blue Detected on vertical')
+        if np.greater(random_pixel_vert[2], 255-E):
+            print('Blue detected on vertical')
 
-        if average_color_horz[:,0] > 255-E:
-            print('Red detected on horizontal')
-        elif average_color_horz[:,1] > 255-E:
-            print('Green detected on horizontal')
-        elif average_color_horz[:,2] > 255-E:
-            print('Blue Detected on horizontal')
+        random_pixel_horz = horz_slice[int(np.random.rand()*10), int(np.random.rand()*10), :]
+
+        if np.greater(random_pixel_horz[0], 255-E):
+            print('Red detected on vertical')
+        if np.greater(random_pixel_horz[1], 255-E):
+            print('Green detected on vertical')
+        if np.greater(random_pixel_horz[2], 255-E):
+            print('Blue detected on vertical')
 
         cv2.imshow('Cam View',  cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
 
-        cv2.imshow('Horz Slice', cv2.cvtColor(horzslice, cv2.COLOR_BGR2RGB))
+        cv2.imshow('Horz Slice', cv2.cvtColor(horz_slice, cv2.COLOR_BGR2RGB))
 
-        cv2.imshow('Vert Slice', cv2.cvtColor(vertslice, cv2.COLOR_BGR2RGB))
+        cv2.imshow('Vert Slice', cv2.cvtColor(vert_slice, cv2.COLOR_BGR2RGB))
 
         if cv2.waitKey(10) == ord('q'):
             break
